a1c6899b9981a7ed2e6175d79311a1bffa367d66
kanhaiya <kanhaiya2778@gmail.com>
2022-09-07 15:41:44 +0530

    iframe modal

---
+++ src/Components/NFTModal.js
@@ -0,0 +1,35 @@
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '0',
+    left: '0',
+    right: 'auto',
+    bottom: 'auto',
+    width: "100%",
+    height: "100%"
+  },
+  overlay:{
+    zIndex:1031
+  }
+};
+
+// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement('#root');
+
+const NFTModal=({url,showModal,setShowModal})=>{
+    const closeModal=()=>{
+        setShowModal(false)
+    }
+    return(
+        <Modal isOpen={showModal}
+        // onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal">
+            <button className="nft-modal-cross-btn" onClick={closeModal}><img src="./assets/images/cross.svg" alt="" /></button>
+            <iframe src={url} frameborder="0" width="100%" height="100%"></iframe>
+        </Modal>
+    )
+}
+export default NFTModal;
--- src/Components/NFTSection.js
+++ src/Components/NFTSection.js
@@ -1,29 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Modal from 'react-modal';
+import NFTModal from './NFTModal';
+import "./NFTSection.css"
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
 
+// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement('#root');
 const NFTSection = (props) => {
 
+    const [nftUrl,setNftUrl]= useState("")
+    const [showModal,setShowModal]= useState(false)
+
+const nftData=[{
+    image:"../assets/images/shoe-thumb/01.png",
+    iframeLink:"https://app.vectary.com/p/33UdpimCRuQZuQQL66B21p",
+},{
+    image:"../assets/images/shoe-thumb/02.png",
+    iframeLink:"https://app.vectary.com/p/3wy3jxchbGO4w9CgyBINyS",
+},{
+    image:"../assets/images/shoe-thumb/01.png",
+    iframeLink:"https://app.vectary.com/p/2JU2I6vNFeeVIVvyy1bZAE",
+},{
+    image:"../assets/images/shoe-thumb/01.png",
+    iframeLink:"https://app.vectary.com/p/4JtWSuEonRWuU02LyNNtlr",
+},{
+    image:"../assets/images/shoe-thumb/01.png",
+    iframeLink:"https://app.vectary.com/p/2Ms4zNoKquPRSNNyeubI0l",
+},{
+    image:"../assets/images/shoe-thumb/01.png",
+    iframeLink:"https://app.vectary.com/p/1aElzxjuWZNe1OSFrJDq6J",
+},
+]
+const handleClick=(url)=>{
+    console.log(url);
+    setNftUrl(url);
+    setShowModal(true);
+}
     return(
+        <>
     <Container>
         <Row className="py-5 nft-section text-center align-items-center d-none d-md-block">
         <h2 className="nevan section-heading py-2 py-md-3">SNEAKER NFTs</h2>
 <div className='row row-cols-3 g-5'>
+{
+    nftData.map((nft,i)=>{
 
-<Image src='../assets/images/shoe-thumb/01.png' className='img-fsluid' onClick={props.handleShow}/>
-
-<Image src='../assets/images/shoe-thumb/02.png' className='img-fsluid'/>
-<Image src='../assets/images/shoe-thumb/03.png' className='img-fluid'/>
-<Image src='../assets/images/shoe-thumb/04.png' className='img-fluid'/>
-<Image src='../assets/images/shoe-thumb/05.png' className='img-fluid'/>
-<Image src='../assets/images/shoe-thumb/06.png' className='img-fluid'/>
-
+        return <Image src={nft.image} className="img-fluid" onClick={()=>{
+            handleClick(nft.iframeLink) 
+        }}   key={i} />
+    })
+}
 
 </div>
 
 
         </Row>
     </Container>
+    <NFTModal url={nftUrl} showModal={showModal} setShowModal={setShowModal}/>
+    </>
     );
 }
 export default NFTSection;
