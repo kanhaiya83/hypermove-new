e2d675aa357be59760e0509edfef960c2cf251d5
kanhaiya <kanhaiya2778@gmail.com>
2022-11-04 00:27:39 +0530

    add toast

--- src/Pages/Tournaments/components/CreateSection.js
+++ src/Pages/Tournaments/components/CreateSection.js
@@ -75,11 +75,11 @@ const CreateSection = ({ setTournaments }) => {
       setTournaments((prev) => [...prev, response.savedTournament]);
       setUserData(response.user);
       setFetching(false);
-      toast.update(id, { render: "Tournament created successfully!!", type: "success",theme:"dark",autoClose:2000,hideProgressBar:true,closeButton:true});
+      toast.update(id, { render: "Tournament created successfully!!", type: "success",theme:"dark",autoClose:2000,hideProgressBar:true,progress:undefined,closeButton:true});
 resetForm()
       return;
     }
-      toast.update(id, { render: "Some error occurred!!", type: "error",theme:"dark",autoClose:2000,hideProgressBar:true,closeButton:true});
+      toast.update(id, { render: "Some error occurred!!", type: "error",theme:"dark",autoClose:2000,hideProgressBar:true,progress:undefined,closeButton:true});
 
     setFetching(false);
 
