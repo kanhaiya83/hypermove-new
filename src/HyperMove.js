5d4e74e78082967210187a54a645719749c2c583
kanhaiya <kanhaiya2778@gmail.com>
2022-11-03 15:26:19 +0530

    style changes

--- src/HyperMove.js
+++ src/HyperMove.js
@@ -8,7 +8,7 @@ import {
   } from "react-router-dom";
 import './HyperMove.css';
 import PlayToEarn from "./Pages/PlayToEarn";
-import HomePage from "./Pages/Home/Home";
+// import HomePage from "./Pages/Home/Home";
 import AlertComponent from "./Components/Alert";
 import ComingSoon from "./Pages/ComingSoon";
 import "./index.scss"
@@ -23,7 +23,7 @@ const HyperMove = (props) => {
 
 <Header/>
 <Routes>
-<Route path="/" element={<HomePage/>}/>
+<Route path="/" element={"<HomePage/>"}/>
 <Route path="/play-to-earn/*" element={<PlayToEarn/>}/>
 <Route path="/refer" element={<ReferralPage/>}/>
 <Route path="/profile" element={<ProfilePage/>}/>
