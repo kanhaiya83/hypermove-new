f1225afc9a8f03b0cb6772306a2122383af24b20
kanhaiya <kanhaiya2778@gmail.com>
2022-11-28 02:10:27 +0530

    delete similar images and add social media handles \

--- src/Pages/Launchpad/index.js
+++ src/Pages/Launchpad/index.js
@@ -15,29 +15,8 @@ import { useBuy } from "../../hooks/useBuy";
 import { injected } from "../../connector";
 import { useSwitchNetwork } from "../../hooks/useSwitchNetwork";
 import { divide, round } from "lodash";
-const NFTGameImages = [
-  "https://user-images.githubusercontent.com/76777058/203996529-bdd6e284-0303-4aaa-9235-05b88b30223a.png",
+import { socialMediaHandles,NFTGameImages ,metaverseImages } from "../../assets";
 
-  "https://user-images.githubusercontent.com/76777058/203996642-0e5a6094-b774-4d96-8713-23b5c199d687.png",
-  "https://user-images.githubusercontent.com/76777058/203996654-e1440887-903e-45af-8d04-29f8eaba7d49.png",
-  "https://user-images.githubusercontent.com/76777058/203996668-836044eb-a5b4-41bc-bbbc-5b61d68c6184.png",
-  "https://user-images.githubusercontent.com/76777058/203996680-8782520c-315b-4dc1-816d-4e97518d323e.png",
-  "https://user-images.githubusercontent.com/76777058/203996688-75cfd930-0091-45c2-84b0-529bbe5ff6a0.png",
-  "https://user-images.githubusercontent.com/76777058/203996704-b6676e5a-d558-408a-bbd7-baa11cd21532.png",
-  "https://user-images.githubusercontent.com/76777058/203996718-eda243bd-ceaf-4c14-b858-1342ff1baea3.png",
-  "https://user-images.githubusercontent.com/76777058/203996723-ebb0fdf0-24da-461b-baa0-99969a213428.png",
-  "https://user-images.githubusercontent.com/76777058/203996726-31422676-b1ba-466d-baf4-7832c14b9aaf.png",
-  "https://user-images.githubusercontent.com/76777058/203996735-27e3e6d7-c7d6-49cb-9b6a-8c4640a6c064.png",
-  "https://user-images.githubusercontent.com/76777058/203996741-0dc9b0a5-e850-47d2-8264-996f15468bfc.png",
-  "https://user-images.githubusercontent.com/76777058/203996747-6dc98b95-c78f-496b-94d9-015729cc07ab.png",
-  "https://user-images.githubusercontent.com/76777058/203996751-f7a5a1c2-bb5d-4fd7-b32f-dba06a7ce439.png",
-];
-const metaverseImages = [
-  "https://user-images.githubusercontent.com/76777058/203997058-c8f94f93-49d8-4fda-b7c0-8f115e7fd8a9.jpg",
-  "https://user-images.githubusercontent.com/76777058/203997065-61e0ab0b-fdaf-4037-ac3d-19595271d4c4.jpg",
-  "https://user-images.githubusercontent.com/76777058/203997067-598c3b27-0fef-4124-9704-c9a1eb604b01.jpg",
-  "https://user-images.githubusercontent.com/76777058/203997073-2de0342b-d524-46dc-b7e1-d10594a5691d.jpg",
-];
 const settings = {
   dots: true,
   infinite: true,
@@ -134,41 +113,41 @@ const LaunchpadProjectPage = () => {
               <div className="social-media-btns">
                 <a
                   className="social-btn"
-                  href="https://twitter.com"
+                  href={socialMediaHandles.twitter}
                   target="_blank"
                 >
                   <img
-                    src="https://user-images.githubusercontent.com/76777058/203913992-146fe30a-a80a-4a5f-887d-c0c54b898427.svg"
+                    src="/assets/images/icons/twitter.svg"
                     alt=""
                   />
                 </a>
                 <a
                   className="social-btn"
-                  href="https://twitter.com"
+                  href={socialMediaHandles.discord}
                   target="_blank"
                 >
                   <img
-                    src="https://user-images.githubusercontent.com/76777058/203914896-b7fda108-336e-4cb9-ad64-eac05c9813c7.svg"
+                    src="/assets/images/icons/discord.svg"
                     alt=""
                   />
                 </a>
                 <a
                   className="social-btn"
-                  href="https://twitter.com"
+                  href={socialMediaHandles.telegram}
                   target="_blank"
                 >
                   <img
-                    src="https://user-images.githubusercontent.com/76777058/203914900-7c96ade9-9014-4323-aa4e-eb7ebe5efd85.svg"
+                    src="/assets/images/icons/telegram.svg"
                     alt=""
                   />
                 </a>
                 <a
                   className="social-btn"
-                  href="https://twitter.com"
+                  href={socialMediaHandles.medium}
                   target="_blank"
                 >
                   <img
-                    src="https://user-images.githubusercontent.com/76777058/203914910-7747f0a8-7514-43cf-b7d1-40859f86ddbc.svg"
+                    src="/assets/images/icons/medium.svg"
                     alt=""
                   />
                 </a>
@@ -326,7 +305,7 @@ const LaunchpadProjectPage = () => {
           <h1>Metaverse</h1>
           <Slider {...settings}>
             {metaverseImages.map((source, i) => {
-               return (
+              return (
                 <div key={i}>
                   <div className="slide-image-container">
                     <div className="img-wrapper">
@@ -334,7 +313,7 @@ const LaunchpadProjectPage = () => {
                     </div>
                   </div>
                 </div>
-             );
+              );
             })}
           </Slider>
         </div>
@@ -353,7 +332,6 @@ const InputContainer = ({
 }) => {
   const { active, account, chainId, activate } = useWeb3React();
   const switchChain = useSwitchNetwork();
-
 
   const tryActivate = async () => {
     try {
@@ -402,7 +380,7 @@ const InputContainer = ({
       </div>
     );
   }
- return (
+  return (
     <div className="form-container  ">
       <div className="deposit-details">
         <h3>Your Deposited</h3>
@@ -423,7 +401,7 @@ const InputContainer = ({
       />
       <div className="approve-btn">
         <button
-        className={!approval.isApprovalRequired && "approved"}
+          className={!approval.isApprovalRequired && "approved"}
           onClick={() => approval.triggeredApproval()}
           disabled={!approval?.isApprovalRequired}
         >
@@ -433,17 +411,16 @@ const InputContainer = ({
               : "Approve"
             : "Approved"}
         </button>
-       {
-        !Boolean(approval.isApprovalRequired) &&
-        <>
-         <button
-          onClick={() => buy.buyToken(amount)}
-          disabled={approval?.isApprovalRequired}
-        >
-          {buy.isLoading ? "Buying..." : "buy"}
-        </button>
-        </>
-       }
+        {!Boolean(approval.isApprovalRequired) && (
+          <>
+            <button
+              onClick={() => buy.buyToken(amount)}
+              disabled={approval?.isApprovalRequired}
+            >
+              {buy.isLoading ? "Buying..." : "buy"}
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
---
+++ src/assets/index.js
@@ -0,0 +1,21 @@
+export const socialMediaHandles = {
+    twitter:"https://twitter.com/intent/follow?screen_name=hypermove_io",
+    discord:"https://discord.com/invite/mMWPVXxAXK",
+    telegram:"https://t.me/Hypermove_io",
+    medium:"https://medium.com/@hypermove"
+}
+export const NFTGameImages = [
+    "https://user-images.githubusercontent.com/76777058/203996529-bdd6e284-0303-4aaa-9235-05b88b30223a.png",
+    "https://user-images.githubusercontent.com/76777058/203996723-ebb0fdf0-24da-461b-baa0-99969a213428.png",
+    "https://user-images.githubusercontent.com/76777058/203996680-8782520c-315b-4dc1-816d-4e97518d323e.png",
+    "https://user-images.githubusercontent.com/76777058/203996741-0dc9b0a5-e850-47d2-8264-996f15468bfc.png",
+    "https://user-images.githubusercontent.com/76777058/203996718-eda243bd-ceaf-4c14-b858-1342ff1baea3.png",
+    "https://user-images.githubusercontent.com/76777058/203996735-27e3e6d7-c7d6-49cb-9b6a-8c4640a6c064.png",
+    "https://user-images.githubusercontent.com/76777058/203996751-f7a5a1c2-bb5d-4fd7-b32f-dba06a7ce439.png",
+  ];
+  export const metaverseImages = [
+    "https://user-images.githubusercontent.com/76777058/203997058-c8f94f93-49d8-4fda-b7c0-8f115e7fd8a9.jpg",
+    "https://user-images.githubusercontent.com/76777058/203997067-598c3b27-0fef-4124-9704-c9a1eb604b01.jpg",
+    "https://user-images.githubusercontent.com/76777058/203997065-61e0ab0b-fdaf-4037-ac3d-19595271d4c4.jpg",
+    "https://user-images.githubusercontent.com/76777058/203997073-2de0342b-d524-46dc-b7e1-d10594a5691d.jpg",
+  ];
